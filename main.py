60de68659d097e855d9723405443fa30b94660fe
shoval <shoval123055@gmail>
2023-03-06 13:01:54 +0200

    fix

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from flask import Flask
 import datetime 
 app = Flask(__name__)
 
-def replace_placeholders(content):
+def replace_placeholders(content,day_of_week,current_date):
     content = content.replace("[DAY_OF_WEEK]", day_of_week)
     content = content.replace("[CURRENT_DATE]", current_date)
     return content
 
 @app.route('/')
 def listen():
-    with open("file.txt", "r") as f:
+    with open("fileListen.txt", "r") as f:
         file_content = f.read()
     # Takes the day from the time zone running on the machine
     day = datetime.datetime.now().strftime("%A")
     # Takes the date from the time zone running on the machine
     date = datetime.datetime.now().strftime("%d.%m.%Y")
     # Prepare
-    res=replace_placeholders(file_content)
+    res=replace_placeholders(file_content,day,date)
     return res
 
 
