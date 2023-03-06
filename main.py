61be987a5189c957622e2daa6bb0e5abd92bc138
shoval <shoval123055@gmail>
2023-03-06 12:59:10 +0200

    add placeholders

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from flask import Flask
 import datetime 
 app = Flask(__name__)
 
+def replace_placeholders(content):
+    content = content.replace("[DAY_OF_WEEK]", day_of_week)
+    content = content.replace("[CURRENT_DATE]", current_date)
+    return content
+
 @app.route('/')
 def listen():
+    with open("file.txt", "r") as f:
+        file_content = f.read()
     # Takes the day from the time zone running on the machine
     day = datetime.datetime.now().strftime("%A")
     # Takes the date from the time zone running on the machine
     date = datetime.datetime.now().strftime("%d.%m.%Y")
     # Prepare
-    update = "Today is "+day+"\nThe current date is "+date
-    # Insert
-    with open("fileListen.txt", "w") as f:
-        f.write(update)
-    return update
+    res=replace_placeholders(file_content)
+    return res
 
 
 if __name__ == "__main__":
